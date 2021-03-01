6334a8dbe3bdacdeb78c0d31a19777cb6aebdf4a
eric-6 <nyaeric237@gmail.com>
2021-02-28 22:37:31 -0500

    Add project files.

---
+++ EmployeePractise/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace EmployeePractise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int s;
+
+          
+
+
+            Console.WriteLine("enter number 1");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter number 2");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            s = a + b;
+            Console.WriteLine($" The sum is "  + s);
+            
+           
+
+         
+
+
+           
+            
+
+        }
+    }
+}
